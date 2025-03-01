5487ddc29ab37fe65fa29caf1e88478e0995c2a0
magda.peter <magda.peter@ckik.hu>
2025-03-01 22:07:17 +0100

    2/b probalkozas

--- 1. fordulo/A/A/Program.cs
+++ 1. fordulo/A/A/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace szavak
 {
@@ -22,6 +23,8 @@ namespace szavak
             }
 
             Console.WriteLine(palindromok.Count);
+            NeveloTav(szavak);
+            Console.ReadKey();
         }
 
         private static bool PalindromE(string szo)
@@ -54,6 +57,39 @@ namespace szavak
                     szavak.Add(szo);
                 }
             }
+        }
+
+        static void NeveloTav(List<string> szavak)
+        {
+            List<int> hosszak = new List<int>(1000);
+
+            int db = 0;
+            int i = 0;
+            int k = 0;
+
+            for (i = 0; i < szavak.Count; i++)
+            {
+                if (szavak[i] == "A" || szavak[i] =="AZ")
+                {
+                    db = i + 1;
+                }
+                while (szavak[db] != "A" || szavak[db] != "AZ")
+                {
+                    hosszak[k] += szavak[db].Length + 1;
+                    db++;
+                }
+                db = 0;
+            }
+
+            int max = hosszak[i];
+            for (int m = 0; m < hosszak.Count; m++)
+            {
+                if (hosszak[i] > max)
+                {
+                    max = hosszak[i];
+                }
+            }
+            Console.WriteLine(max);
         }
     }
 }
